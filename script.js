8a51cbad14df16855833d9344a49e1563e4c4c9c
Cameron Samuels <me@cameronsamuels.com>
2018-05-07 20:35:27 -0500

    (update)Fixed to @Nova281's request

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-/*var points = 0;
-	function clickFunction(amount) {
-		if(points + amount < 0) {
-			amounts = 0;
-		}
-		points = points + amount;
-		document.querySelector("p").textContent = points + " V Bucks";*/
-	document.getElementById('imgButton').addEventListener('click', function(){
-  		alert('Works!', this.name);
-	});
+var points = 0;
+function clickFunction() {
+	points++;
+	document.querySelector("p").textContent = points + " V-Bucks";
+}
+document.getElementById('imgButton').addEventListener('click', clickFunction);
